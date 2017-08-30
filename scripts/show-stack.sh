38dae9e6f29be73bc5985321518412b21e88b8ba
Jingwei Peng <jay.peng@inlight.com.au>
2017-08-30 13:52:17 +1000

    Also disable default aws profile for show and validate.

--- scripts/show-stack.sh
+++ scripts/show-stack.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
+AWS_PROFILE="${AWS_PROFILE:?\"Missing AWS_PROFILE environment variable -- will not use default profile!\"}"
 aws cloudformation describe-stacks --stack-name $1
--- scripts/validate-template.sh
+++ scripts/validate-template.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
+AWS_PROFILE="${AWS_PROFILE:?\"Missing AWS_PROFILE environment variable -- will not use default profile!\"}"
 aws cloudformation validate-template --template-body "file://$1"
